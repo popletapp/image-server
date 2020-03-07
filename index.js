1454c2b04eeedd38ca272c932cdf1e8fc2fcf8c2
Blake <bsward8@gmail.com>
2020-03-08 09:05:16 +1100

    feat: improvements to image server

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const upload = require('express-fileupload');
 const fs = require('fs');
 const config = require('./config.json');
 
+// CORS
+router.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://popletapp.com');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Super-Properties, X-Context-Properties, X-Failed-Requests, If-None-Match');
+  res.setHeader('Access-Control-Max-Age', '86400')
+  next();
+});
+
 router.use(
   upload({
     safeFileNames: true,
@@ -17,8 +27,8 @@ router.use(
 const ROUTES = ['avatars', 'attachments'];
 for (const route of ROUTES) {
   const mod = require(`./routes/${route}`);
-  router.use(`/${route}`, express.static(`./uploads/${route}`));
   router.use(`/${route}`, mod)
+  router.use(`/${route}`, express.static(`${__dirname}/uploads/${route}`));
 }
 
 module.exports = router;
--- upload.js
+++ upload.js
@@ -1,11 +1,9 @@
 const config = require('./config.json');
-const keys = config.keys;
 const generateUUID = require('./utils/generateUUID');
 const path = require('path');
 const fs = require('fs');
 
 module.exports = (req, res, type = 'attachments', id = '') => {
-  const key = req.headers.authorization;
   if (!fs.existsSync(`${__dirname}/uploads/`)) {
     fs.mkdirSync(`${__dirname}/uploads/`);
   }
@@ -18,17 +16,14 @@ module.exports = (req, res, type = 'attachments', id = '') => {
     fs.mkdirSync(`${__dirname}/uploads/${type}/${id}/`);
   }
 
-  if (keys.indexOf(key) === -1) {
-    return res.status(401).json({ message: 'Authentication failed' });
-  }
-
-  if (!req.files.file) {
+  if (req.files && !req.files.file) {
     return res.status(400).json({ message: 'No attachments present' });
   }
 
   const file = req.files.file;
   const fileExtension = path.extname(file.name);
-  const newFileName = `${generateUUID()}${fileExtension}`;
+  const hash = generateUUID();
+  const newFileName = `${hash}${fileExtension}`;
   const uploadPath = id ? `${__dirname}/uploads/${type}/${id}/${newFileName}` : `${__dirname}/uploads/${type}/${newFileName}`;
 
   if (config.limits[type] && file.size > config.limits[type]) {
@@ -40,8 +35,8 @@ module.exports = (req, res, type = 'attachments', id = '') => {
   }
   file.mv(uploadPath, (err) => {
       if (err) {
-          console.error(err);
-          return res.status(500).json({ message: 'An error occured whilst trying to save the file.' });
+        console.error(err);
+        return res.status(500).json({ message: 'An error occured whilst trying to save the file.' });
       }
       console.log('Uploaded file ' + file.name + ' to ' + newFileName);
       res.status(200).json({
